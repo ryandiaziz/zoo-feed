1856dbb14f2abe34422ceb60b3c615df1ebef59b
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-03-24 15:16:45 +0700

    create and migrate db, associate models

---
+++ models/animal.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class animal extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      animal.belongsTo(models.classType);
+      animal.belongsTo(models.habitat);
+      animal.belongsToMany(models.food, { through: models.animalFood });
+    }
+  }
+  animal.init({
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    sex: DataTypes.STRING,
+    classId: DataTypes.INTEGER,
+    habitatId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'animal',
+  });
+  return animal;
+};
