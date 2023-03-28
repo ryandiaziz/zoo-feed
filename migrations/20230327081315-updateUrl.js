8bcdb55a79bc282b5e279800fc6d2c00fa544766
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-03-28 14:35:23 +0700

    animals detail page

--- migrations/20230327081315-updateUrl.js
+++ migrations/20230327081315-updateUrl.js
@@ -2,29 +2,26 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn('animals', 'imageUrl', {
-      type: Sequelize.STRING})
-
-      await queryInterface.addColumn('food', 'imageUrl', {
-        type: Sequelize.STRING})
+    await queryInterface.addColumn('food', 'imageUrl', {
+      type: Sequelize.STRING
+    })
 
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.removeColumn('animals', 'imageUrl');
     await queryInterface.removeColumn('food', 'imageUrl');
   }
 };
