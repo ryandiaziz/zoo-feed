2ce84f6e3326510b742b801a1c8b10cd5c135657
Azzam Adnan <azzamuadonang@gmail.com>
2023-04-18 13:04:04 +0700

    Home page update

--- client/src/pages/classPages/ClassDetail.js
+++ client/src/pages/classPages/ClassDetail.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useParams } from "react-router";
+import { detailClass } from '../../axios/classType';
 
 const ClassDetail = () => {
+  const params = useParams();
+    
+    const [detailData, setDetaildata] = useState({
+      
+    })
+    
+
+    const getClassDetail = () =>{
+      const {id} =params
+      detailClass(+id,(result)=>{
+        setDetaildata({
+           result})
+      })
+    }
+
+    useEffect(()=>{
+      getClassDetail()
+    },[])
+
   return (
-    <div>classDetail</div>
+    <div></div>
   )
 }
 
